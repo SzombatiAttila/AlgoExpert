7739490cbd04df71655ab1f83ae862e2145c1ed7
MagyarMate <magyarr91@gmail.com>
2020-05-13 23:46:04 +0200

    Update riversizes_dfs_optimised.py
    Added some more optimisation finally more efficient than Bence's solution. In most of the TCs...

--- Graphs/riverSizes/riversizes_dfs_optimised.py
+++ Graphs/riverSizes/riversizes_dfs_optimised.py
@@ -1,33 +1,32 @@
 def riversizes_dfs_optimized(matrix):
     rivers = []
-    stack = []
     for i in range(len(matrix)):
         for j in range(len(matrix[i])):
             if matrix[i][j] == 1:
+                stack = []
                 length = 0
-                first = (i, j)
-                stack.append(first)
+                stack.append((i, j))
                 while len(stack) > 0:
-                    length += matrix[i][j]
                     matrix[i][j] = 0
                     # Go up
                     if i - 1 > -1 and matrix[i - 1][j] == 1:
                         stack.append((i, j))
-                        i = i - 1
+                        i -= 1
                     # Go left
                     elif j - 1 > -1 and matrix[i][j - 1] == 1:
                         stack.append((i, j))
-                        j = j - 1
+                        j -= 1
                     # Go down
                     elif i + 1 < len(matrix) and matrix[i + 1][j] == 1:
                         stack.append((i, j))
-                        i = i + 1
+                        i += 1
                     # Go right
                     elif j + 1 < len(matrix[0]) and matrix[i][j + 1] == 1:
                         stack.append((i, j))
-                        j = j + 1
+                        j += 1
                     else:
                         i, j = stack.pop()
+                        length += 1
                 else:
                     rivers.append(length)
     return rivers

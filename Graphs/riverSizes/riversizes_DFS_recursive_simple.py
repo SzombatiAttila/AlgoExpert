4d1380dd3b9e6bc5eb8fd0294c500afeea75f52e
MagyarMate <magyarr91@gmail.com>
2020-05-13 23:53:50 +0200

    Update riversizes_DFS_recursive_simple.py
    Even more simplification.

--- Graphs/riverSizes/riversizes_DFS_recursive_simple.py
+++ Graphs/riverSizes/riversizes_DFS_recursive_simple.py
@@ -3,15 +3,13 @@ def riversizes_dfs(matrix):
 
     def appendRiver(i, j):
         if 0 <= i < len(matrix) and 0 <= j < len(matrix[0]) and matrix[i][j] == 1:
-            matrix[i][j] += 1
+            matrix[i][j] = 0
             return 1 + appendRiver(i - 1, j) + appendRiver(i, j - 1) + appendRiver(i + 1, j) + appendRiver(i, j + 1)
-        else:
-            return 0
+        return 0
 
-    for i, row in enumerate(matrix):
-        for j, _ in enumerate(row):
-            river = appendRiver(i, j)
-            if river > 0:
-                rivers.append(river)
+    for i in range(len(matrix)):
+        for j in range(len(matrix[i])):
+            if matrix[i][j] == 1:
+                rivers.append(appendRiver(i, j))
 
     return rivers
